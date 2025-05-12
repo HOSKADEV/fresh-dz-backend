11bde5408709ba932ddb5d40261ba3bd498d9edb
AbdelaliKDM <khademallah.abdelali@gmail.com>
2025-05-12 14:36:11 +0100

    add playstore app link

--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -6,7 +6,7 @@
         var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
         if (/android/i.test(userAgent)) {
-            window.location.href = "https://play.google.com/store/apps/details?id=com.example.app";
+            window.location.href = "https://play.google.com/store/apps/details?id=com.fresh.dz&hl=en";
         } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
             window.location.href = "https://apps.apple.com/app/id123456789";
         } else {
