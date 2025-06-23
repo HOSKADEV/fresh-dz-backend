4f0d58662b8459c46acdf4c3e28f8ece14a3d47a
AbdelaliKDM <khademallah.abdelali@gmail.com>
2025-06-23 22:54:34 +0100

    fix 2

--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -249,11 +249,7 @@ class DiscountController extends Controller
 
       $discounts = DB::table('discounts')
         ->WhereRaw('? between start_date and end_date', Carbon::now()->toDateString())
-        //->join('products', 'discounts.product_id', 'products.id')
-        ->join('products', function ($join) {
-          $join->on('discounts.product_id', '=', 'products.id')
-            ->whereNotNull('products.image');
-        })
+        ->join('products', 'discounts.product_id', 'products.id')
         ->join('subcategories', 'products.subcategory_id', 'subcategories.id')
         ->join('categories', 'subcategories.category_id', 'categories.id')
         ->orderBy('categories.created_at', 'DESC')
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -42,7 +42,8 @@ class Category extends Model
     }
 
     public function discounts(){
-      $products = Product::whereIn('subcategory_id',$this->subcategories()->pluck('id')->toArray())
+      $products = Product::whereNotNull('products.image')
+      ->whereIn('subcategory_id',$this->subcategories()->pluck('id')->toArray())
       ->join('discounts','products.id','discounts.product_id')
       ->WhereRaw('? between start_date and end_date', Carbon::now()->toDateString())->where('discounts.deleted_at',null)
       ->select('products.*','discounts.id as discount_id','discounts.amount','discounts.start_date','discounts.end_date')
